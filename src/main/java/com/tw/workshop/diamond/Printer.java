e2765fce0f64f6c5d028a8bd91b89e5210f6ad9e
Xiaofeng Wang <xfun68@gmail.com>
2017-07-12 18:19:04 +0800

    extract getSeq() and getLine()

--- src/main/java/com/tw/workshop/diamond/Printer.java
+++ src/main/java/com/tw/workshop/diamond/Printer.java
@@ -1,16 +1,54 @@
 package com.tw.workshop.diamond;
 
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Printer {
     public List<String> diamond(String letter) {
+        ArrayList<String> lines = new ArrayList<>();
+        for (String lineLetter : getSeq(letter)) {
+            lines.add(getLine(letter, lineLetter));
+        }
+        return lines;
+    }
+
+    private List<String> getSeq(String letter) {
+        List<String> firstHalf = new ArrayList<>();
+        for (char c = 'A'; c <= letter.charAt(0); c++) {
+            firstHalf.add(String.valueOf(c));
+        }
+
+        if (firstHalf.size() <= 1) {
+            return firstHalf;
+        }
+
+        List<String> secondHalf = new ArrayList<>(firstHalf.subList(0, firstHalf.size() - 1));
+        Collections.reverse(secondHalf);
+
+        List<String> result = new ArrayList<>();
+        result.addAll(firstHalf);
+        result.addAll(secondHalf);
+        return result;
+    }
+
+    private String getLine(String letter, String lineLetter) {
         if (letter.equals("A")) {
-            return Arrays.asList("A");
+            return "A";
         } else if (letter.equals("B")) {
-            return Arrays.asList(" A ", "B B", " A ");
+            if (lineLetter.equals("A")) {
+                return " A ";
+            } else {
+                return "B B";
+            }
         } else {
-            return Arrays.asList("  A  ", " B B ", "C   C", " B B ", "  A  ");
+            if (lineLetter.equals("A")) {
+                return "  A  ";
+            } else if (lineLetter.equals("B")) {
+                return " B B ";
+            } else {
+                return "C   C";
+            }
         }
     }
 }
